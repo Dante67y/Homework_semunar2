cc1bdcf564dfda361a2ccaccc6e0969b5e92448a
Dante67yt <dante67yt@gmail.com>
2022-06-28 17:20:21 +0300

    add task 3 + оформил 'интерфейс'

---
+++ task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 6 || n % 7 == 0) Console.WriteLine("УРА! Выходной!");
+else Console.WriteLine ("Рабочий...");
+
+//int n = Convert.ToInt32(Console.ReadLine());
+//if (n % 7 == 1) Console.WriteLine("Понедельник");
+//if (n % 7 == 2) Console.WriteLine("Вторник");
+//if (n % 7 == 3) Console.WriteLine("Среда");
+//if (n % 7 == 4) Console.WriteLine("Четверг");
+//if (n % 7 == 5) Console.WriteLine("Пятница");
+//if (n % 7 == 6) Console.WriteLine("Суббота");
+//if (n % 7 == 0) Console.WriteLine("Воскресенье");
