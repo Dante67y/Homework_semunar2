70783346d4ac10cab7b0777c139ee1d8fec4b3f0
Dante67yt <dante67yt@gmail.com>
2022-06-28 17:20:03 +0300

    оформил 'интерфейс'

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,25 +1,10 @@
-﻿int n;
-Console.Write("Введите число ");
-n = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
 if (n < 100) Console.WriteLine("Третьей цифры нет");
-else {
-    if (n > 99 && n < 1000) Console.WriteLine(n % 10);
-    else {
-        if (n > 999 && n < 10000) Console.WriteLine((n % 100) / 10);
-        else {
-            if (n > 9999 && n < 100000) Console.WriteLine((n % 1000) / 100);
-            else {
-                if (n > 99999 && n < 1000000) Console.WriteLine((n % 10000) / 1000);
-                else {
-                    if (n > 999999 && n < 10000000) Console.WriteLine((n % 100000) / 10000);
-                    else {
-                        if (n > 9999999 && n < 100000000) Console.WriteLine((n % 1000000) / 100000);
-                        else {
-                            if (n > 99999999 && n < 1000000000) Console.WriteLine((n % 10000000) / 1000000);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+if (n > 99 && n < 1000) Console.WriteLine(n % 10);
+if (n > 999 && n < 10000) Console.WriteLine((n % 100) / 10);
+if (n > 9999 && n < 100000) Console.WriteLine((n % 1000) / 100);
+if (n > 99999 && n < 1000000) Console.WriteLine((n % 10000) / 1000);
+if (n > 999999 && n < 10000000) Console.WriteLine((n % 100000) / 10000);
+if (n > 9999999 && n < 100000000) Console.WriteLine((n % 1000000) / 100000);
+if (n > 99999999 && n < 1000000000) Console.WriteLine((n % 10000000) / 1000000);
