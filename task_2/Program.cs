88e405f9e9854a753b9813c4b29d9f27e6055408
Dante67yt <dante67yt@gmail.com>
2022-06-28 17:09:19 +0300

    add task 2 для трехзначных, четыре значных и так далее, пришлось прописывать свли условия

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int n;
+Console.Write("Введите число ");
+n = Convert.ToInt32(Console.ReadLine());
+if (n < 100) Console.WriteLine("Третьей цифры нет");
+else {
+    if (n > 99 && n < 1000) Console.WriteLine(n % 10);
+    else {
+        if (n > 999 && n < 10000) Console.WriteLine((n % 100) / 10);
+        else {
+            if (n > 9999 && n < 100000) Console.WriteLine((n % 1000) / 100);
+            else {
+                if (n > 99999 && n < 1000000) Console.WriteLine((n % 10000) / 1000);
+                else {
+                    if (n > 999999 && n < 10000000) Console.WriteLine((n % 100000) / 10000);
+                    else {
+                        if (n > 9999999 && n < 100000000) Console.WriteLine((n % 1000000) / 100000);
+                        else {
+                            if (n > 99999999 && n < 1000000000) Console.WriteLine((n % 10000000) / 1000000);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
